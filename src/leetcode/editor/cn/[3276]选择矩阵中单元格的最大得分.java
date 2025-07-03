e2d1382394fb04de8d8a981e51dfdb5a8301ba45
WuYetao <wyetao@chohotech.com>
2025-07-03 19:11:28 +0800

    fix: 3276

---
+++ src/leetcode/editor/cn/[3276]选择矩阵中单元格的最大得分.java
@@ -0,0 +1,75 @@
+//给你一个由正整数构成的二维矩阵 grid。 
+//
+// 你需要从矩阵中选择 一个或多个 单元格，选中的单元格应满足以下条件： 
+//
+// 
+// 所选单元格中的任意两个单元格都不会处于矩阵的 同一行。 
+// 所选单元格的值 互不相同。 
+// 
+//
+// 你的得分为所选单元格值的总和。 
+//
+// 返回你能获得的 最大 得分。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+// 输入： grid = [[1,2,3],[4,3,2],[1,1,1]] 
+// 
+//
+// 输出： 8 
+//
+// 解释： 
+//
+// 
+//
+// 选择上图中用彩色标记的单元格，对应的值分别为 1、3 和 4 。 
+//
+// 示例 2： 
+//
+// 
+// 输入： grid = [[8,7,6],[8,3,2]] 
+// 
+//
+// 输出： 15 
+//
+// 解释： 
+//
+// 
+//
+// 选择上图中用彩色标记的单元格，对应的值分别为 7 和 8 。 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= grid.length, grid[i].length <= 10 
+// 1 <= grid[i][j] <= 100 
+// 
+//
+// Related Topics 位运算 数组 动态规划 状态压缩 矩阵 👍 19 👎 0
+
+  
+package leetcode.editor.cn;
+
+import java.util.List;
+
+class SelectCellsInGridWithMaximumScore_3276{
+
+  public static void main(String[] args) {
+       Solution solution = new SelectCellsInGridWithMaximumScore_3276().new Solution();
+  }
+  
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int maxScore(List<List<Integer>> grid) {
+        return 0;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+ 
+}
