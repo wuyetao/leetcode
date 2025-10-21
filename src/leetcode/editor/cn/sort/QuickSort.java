ebf25df01e28a04bac4db9080dcd2fd5136e262e
WuYetao <wyetao@chohotech.com>
2025-10-21 10:08:23 +0800

    feat: 快速排序

--- src/leetcode/editor/cn/sort/QuickSort.java
+++ src/leetcode/editor/cn/sort/QuickSort.java
@@ -8,7 +8,41 @@ package leetcode.editor.cn.sort;
  */
 public class QuickSort {
 
-    public void sort(int[] nums){
-//        quickSort(nums, 0, nums.length - 1);
+    public void sort(int[] nums, int left, int right){
+        if(left >= right){
+            return;
+        }
+        int pivot = partition(nums, left, right);
+        sort(nums, left, pivot - 1);
+        sort(nums, pivot + 1, right);
+    }
+
+    public void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public int partition(int[] nums, int left, int right){
+        int pivot = left;
+        while(left < right){
+            while(left < right && nums[right] >= nums[pivot]){
+                right--;
+            }
+            while(left < right && nums[left] <= nums[pivot]){
+                left++;
+            }
+            swap(nums, left, right);
+        }
+        swap(nums, left, pivot);
+        return left;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {5,4,8,2,1, 1, 1, 1};
+        new QuickSort().sort(nums, 0, nums.length - 1);
+        for(int i = 0; i < nums.length; i++){
+            System.out.print(nums[i] + " ");
+        }
     }
 }
