00d4d112f91408421b9dc6824ba05044dc396eb9
WuYetao <wyetao@chohotech.com>
2025-11-07 13:47:29 +0800

    fix: 22\23

---
+++ src/leetcode/editor/cn/[22]括号生成.java
@@ -0,0 +1,65 @@
+//数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：n = 3
+//输出：["((()))","(()())","(())()","()(())","()()()"]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：n = 1
+//输出：["()"]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= n <= 8 
+// 
+//
+// Related Topics 字符串 动态规划 回溯 👍 3957 👎 0
+
+  
+package leetcode.editor.cn;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class GenerateParentheses_22{
+
+  public static void main(String[] args) {
+       Solution solution = new GenerateParentheses_22().new Solution();
+  }
+  
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<>();
+        generate("", n, n, res);
+        return res;
+    }
+
+    public void generate(String s, int left, int right, List<String> res){
+        if(left == 0 && right == 0){
+            res.add(s);
+            return;
+        }
+        if(left > 0){
+            generate(s + "(", left - 1, right, res);
+        }
+        if(right > left){
+            generate(s + ")", left, right - 1, res);
+        }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+ 
+}
